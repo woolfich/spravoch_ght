504a342d59fffce0ff8e8904eef4efcb836167f6
evgen <evgen@192.168.1.12>
2026-01-18 14:40:58 +0300

    fix: добавлены иконки и исправлен путь в манифесте

--- sw.js
+++ sw.js
@@ -4,6 +4,8 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c =>
       c.addAll(['./', './index.html', './data.json'])
+         './icons/icon-192.png', // <--- Добавь это
+        './icons/icon-512.png'  // <--- И это
     )
   );
 });
