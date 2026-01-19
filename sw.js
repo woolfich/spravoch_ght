41b5c13a340d5d6c31e275fa4b9bfaefb7aae35a
woolfich <peresver@gmail.com>
2026-01-19 10:50:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,9 +3,13 @@ const CACHE = 'ref-cache-v1';
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c =>
-      c.addAll(['./', './index.html', './data.json'])
-         './icons/icon-192.png', // <--- Добавь это
-        './icons/icon-512.png'  // <--- И это
+      c.addAll([
+        './',
+        './index.html',
+        './data.json',
+        './icons/icon-192.png',
+        './icons/icon-512.png'
+      ])
     )
   );
 });
